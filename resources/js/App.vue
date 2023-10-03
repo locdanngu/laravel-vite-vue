bca243bb855d4d16633c352b011e373fd981c717
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-10-03 10:06:31 +0700

    up

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,41 @@
+<!-- <template>
+Hello, World!
+</template> -->
+
 <template>
-    Hello, World!
+    <div>
+        <h1>Hello, World!</h1>
+        <h2>Danh sách sản phẩm:</h2>
+        <ul>
+            <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+        </ul>
+    </div>
 </template>
+
+  
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            products: [],
+        };
+    },
+    created() {
+        this.fetchProducts();
+    },
+    methods: {
+        fetchProducts() {
+            axios.get('/api/product').then((response) => {
+                this.products = response.data.products;
+            });
+        },
+    },
+};
+</script>
+
+  
+<style>
+/* Thêm kiểu CSS của bạn tại đây */
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hello, World!</title>
 </head>
+
 <body>
     <div id="app">
-        <hello-world></hello-world>
+        <ul>
+            <li v-for="product in products">{{ product.name }}</li>
+        </ul>
     </div>
 
     @vite('resources/js/app.js')
 </body>
+
 </html>
