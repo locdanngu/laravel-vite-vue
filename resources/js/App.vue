7ea0c6f3f3de32ea35e042c9f50bd08ec954e05a
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-10-03 10:29:04 +0700

    up

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -6,11 +6,29 @@ Hello, World!
     <div>
         <h1>Hello, World!</h1>
         <h2>Danh sách sản phẩm:</h2>
-        <ul>
-            <li v-for="product in products" :key="product.id">{{ product.name }}</li>
-        </ul>
+        <table class="product-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Giá cũ</th>
+                    <th>Giá mới</th>
+                    <th>Chi tiết</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products.data" :key="product.idproduct">
+                    <td>{{ product.idproduct }}</td>
+                    <td>{{ product.nameproduct }}</td>
+                    <td>{{ product.oldprice }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.detail }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
+
 
   
 <script>
@@ -28,7 +46,7 @@ export default {
     methods: {
         fetchProducts() {
             axios.get('/api/product').then((response) => {
-                this.products = response.data.products;
+                this.products = response.data;
             });
         },
     },
@@ -37,5 +55,29 @@ export default {
 
   
 <style>
-/* Thêm kiểu CSS của bạn tại đây */
+/* Kiểu dáng bảng */
+.product-table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.product-table th,
+.product-table td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+}
+
+.product-table th {
+    background-color: #f2f2f2;
+}
+
+/* Định dạng dòng chẵn và lẻ của bảng */
+.product-table tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+.product-table tr:nth-child(odd) {
+    background-color: #fff;
+}
 </style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Hello, World!</title>
 </head>
-
 <body>
     <div id="app">
-        <ul>
-            <li v-for="product in products">{{ product.name }}</li>
-        </ul>
+        <list></list>
     </div>
 
     @vite('resources/js/app.js')
 </body>
-
 </html>
