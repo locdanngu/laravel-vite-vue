a48cf9509d4e7c4a9cdfa98cfac59aadc8c2283b
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-10-04 11:09:07 +0700

    up

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,50 +1,55 @@
 <template>
-    <div class="p-5">
-        <div class="d-flex justify-content-between mb-2">
-            <h2>Danh sách sản phẩm:</h2>
-            <input type="text" v-model="searchQuery" placeholder="Tìm kiếm" class="form-control w-25">
-        </div>
-        <div class="card-body table-responsive p-0">
-            <table class="product-table table text-nowrap">
-                <thead>
-                    <tr>
-                        <th rowspan="3">ID</th>
-                        <th rowspan="3">Tên sản phẩm</th>
-                        <th rowspan="3" class="text-center">Hình ảnh</th>
-                        <th rowspan="3">Giá cũ</th>
-                        <th rowspan="3">Giá mới</th>
-                        <th colspan="3" class="text-center">Danh mục</th>
-                        <th colspan="3" class="text-center">Loại hàng</th>
-                    </tr>
-                    <tr>
-                        <th rowspan="2" class="text-center">ID</th>
-                        <th rowspan="2" class="text-center">Danh mục</th>
-                        <th rowspan="2" class="text-center">Ngày tạo</th>
-                        <th rowspan="2" class="text-center">ID</th>
-                        <th rowspan="2" class="text-center">Loại</th>
-                        <th rowspan="2" class="text-center">Ngày tạo</th>
-                    </tr>
-                </thead>
-                <tbody class="align-middle">
-                    <tr v-for="product in products.data" :key="product.idproduct">
-                        <td>{{ product.idproduct }}</td>
-                        <td>{{ product.nameproduct }}</td>
-                        <td class="text-center"><img :src="product.imageproduct" alt="" height="50"></td>
-                        <td>{{ product.oldprice }}</td>
-                        <td>{{ product.price }}</td>
-                        <td class="text-center">{{ product.namecategory.idcategory }}</td>
-                        <td class="text-center">{{ product.namecategory.namecategory }}</td>
-                        <td class="text-center">{{ formattedDate(product.namecategory.created_at) }}</td>
-                        <td class="text-center">{{ product.nametype.idtype }}</td>
-                        <td class="text-center">{{ product.nametype.nametype }}</td>
-                        <td class="text-center">{{ formattedDate(product.nametype.created_at) }}</td>
-
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
+<div class="p-5">
+    <div class="d-flex justify-content-between mb-2">
+        <h2>Danh sách sản phẩm:</h2>
+        <input type="text" v-model="searchQuery" placeholder="Tìm kiếm" class="form-control w-25">
     </div>
+    <div class="card-body table-responsive p-0">
+        <table class="product-table table text-nowrap">
+            <thead>
+                <tr>
+                    <th rowspan="3">ID</th>
+                    <th rowspan="3">Tên sản phẩm</th>
+                    <th rowspan="3" class="text-center">Hình ảnh</th>
+                    <th rowspan="3">Giá cũ</th>
+                    <th rowspan="3">Giá mới</th>
+                    <th colspan="3" class="text-center">Danh mục</th>
+                    <th colspan="3" class="text-center">Loại hàng</th>
+                </tr>
+                <tr>
+                    <th rowspan="2" class="text-center">ID</th>
+                    <th rowspan="2" class="text-center">Danh mục</th>
+                    <th rowspan="2" class="text-center">Ngày tạo</th>
+                    <th rowspan="2" class="text-center">ID</th>
+                    <th rowspan="2" class="text-center">Loại</th>
+                    <th rowspan="2" class="text-center">Ngày tạo</th>
+                </tr>
+            </thead>
+            <tbody class="align-middle">
+                <tr v-for="product in products" :key="product.idproduct">
+                    <td>{{ product.idproduct }}</td>
+                    <td>{{ product.nameproduct }}</td>
+                    <td class="text-center"><img :src="product.imageproduct" alt="" height="50"></td>
+                    <td>{{ product.oldprice }}</td>
+                    <td>{{ product.price }}</td>
+                    <td class="text-center">{{ product.namecategory.idcategory }}</td>
+                    <td class="text-center">{{ product.namecategory.namecategory }}</td>
+                    <td class="text-center">{{ formattedDate(product.namecategory.created_at) }}</td>
+                    <td class="text-center">{{ product.nametype.idtype }}</td>
+                    <td class="text-center">{{ product.nametype.nametype }}</td>
+                    <td class="text-center">{{ formattedDate(product.nametype.created_at) }}</td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="pagination">
+        <button @click="previousPage" :disabled="currentPage === 1">Previous</button>
+        <span>Page {{ currentPage }} of {{ lastPage }}</span>
+        <button @click="nextPage" :disabled="currentPage === lastPage">Next</button>
+    </div>
+</div>
 </template>
 
 <script>
@@ -56,6 +61,8 @@ export default {
         return {
             products: [],
             searchQuery: '',
+            currentPage: 1, // Trang hiện tại
+            lastPage: 1,
         };
     },
     created() {
@@ -63,15 +70,34 @@ export default {
     },
     methods: {
         fetchProducts() {
+            const currentPage = parseInt(this.$route.query.page) || 1;
+            
             let apiUrl = '/api/product';
 
             if (this.searchQuery) {
                 apiUrl += `?search=${this.searchQuery}`;
+                apiUrl += `&page=${this.currentPage}`;
+            }else{
+                apiUrl += `?page=${this.currentPage}`;
             }
-
+            
             axios.get(apiUrl).then((response) => {
-                this.products = response.data;
+                this.products = response.data.data;
+                this.lastPage = response.data.last_page;
+                this.$router.replace({ query: { page: this.currentPage } });
             });
+        },
+        previousPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+                this.fetchProducts();
+            }
+        },
+        nextPage() {
+            if (this.currentPage < this.lastPage) {
+                this.currentPage++;
+                this.fetchProducts();
+            }
         },
     },
     computed: {
@@ -84,6 +110,7 @@ export default {
             this.fetchProducts();
         },
     },
+
 };
 </script>
 
